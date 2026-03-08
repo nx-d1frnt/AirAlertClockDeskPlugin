c078135a19c45bfca78a25d7cddb2ab956434467
nx-d1frnt <kam1kadze.official82@gmail.com>
2026-03-08 08:50:31 +0200

    Increment version to 1.2 and update versionCode to 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nxd1frnt.airalertclockdeskplugin"
         minSdk = 21
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
